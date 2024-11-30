48b496fab17a7a188349e432aedd49c879285f69
Martin Knor <martinko.knor@gmail.com>
2024-11-30 12:37:42 +0100

    feat: defined time constants

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
+/* 
+ * @file main.cpp
+ * @author Adam Havlik (xhavli59)
+ * @author Martin Knor (xknorm01)
+ * @date 11.2024
+ * @brief program entry point
+ */
+
 #include <iostream> // Include the input/output stream library
+#include "simlib.h"
+
+
+int MINUTE = 1;             // simulation step equals one minute
+int HOUR = MINUTE * 60;
+int DAY = HOUR * 24;
+int YEAR = DAY * 365;
 
 int main() {
-    // Output "Hello, World!" to the console
-    std::cout << "Hello, World!" << std::endl;
-    return 0; // Indicate that the program ended successfully
+    Init(0, YEAR); // Initialize the simulation
+    Run();        // Run the simulation
+    return 0;
 }
